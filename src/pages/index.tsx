3d2eab51b6d00d2961019c2aa29fab66796d6d67
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-08-02 23:10:53 -0400

    SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
+export default function Home({ episodes }: any) {
   return (
-    <h1>teste</h1>
+    <p>
+      {JSON.stringify(episodes)}
+    </p>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 3
+  }
 }
