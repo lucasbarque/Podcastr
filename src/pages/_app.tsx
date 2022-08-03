7c9b8e27ba0c1295cc4e51c94981d45170ca94e5
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-08-02 20:48:57 -0400

    main: Subindo projeto inicial

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import '../styles/global.scss';
+
+import styles from '../styles/app.module.scss';
+
+import type { AppProps } from 'next/app'
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  )
+}
+
+export default MyApp
